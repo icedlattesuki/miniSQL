f2d57847c4dfcf8699835fcde1f20d28d3f8df39
黄雨生 <yusen@HysdeMacBook-Pro.local>
2017-05-29 19:27:27 +0800

    单元测试文件更新

--- unit_test_for_buffer_manager.cc
+++ unit_test_for_buffer_manager.cc
@@ -1,16 +1,16 @@
 #include "buffer_manager.h"
+using namespace std;
 
 int main() {
     BufferManager buffer_manager;
-    int page_id = buffer_manager.getEmptyPageId();
-    buffer_manager.loadDiskBlock(page_id , "buffer_manager_test" , 0);
+    char* buffer = buffer_manager.getPage("buffer_manager_test" , 0);
+    int page_id = buffer_manager.getPageId("buffer_manager_test" , 0);
     cout << "unpin " << buffer_manager.unpinPage(page_id) << endl;
     cout << "unpin " << buffer_manager.unpinPage(page_id) << endl;    
     buffer_manager.pinPage(page_id);
     cout << "unpin " << buffer_manager.unpinPage(page_id) << endl; 
     cout << "page id " << buffer_manager.getPageId("buffer_manager_test" , 0) << endl;       
-    char* buffer = buffer_manager.getPage(page_id);
-    strcpy(buffer , "I love you");
+    strcpy(buffer , "ZJU is the Best!");
     buffer_manager.modifyPage(page_id);
     buffer_manager.flushPage(page_id , "buffer_manager_test" , 0);
 }
