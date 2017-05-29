c641084264e4bfd6dcd29b6e2645e2e0f8510550
ShengAndXian <huangyusheng01@gmail.com>
2017-05-29 19:26:08 +0800

    更新基本数据结构

---
+++ basic.cc
@@ -0,0 +1,171 @@
+//
+//  basic.cpp
+//  base
+//
+//  Created by Sr on 2017/5/25.
+//  Copyright © 2017年 Sr. All rights reserved.
+//
+
+#include "basic.h"
+
+Tuple::Tuple(const Tuple &tuple_in){
+    for(int index=0;index<tuple_in.data_.size();index++)
+    {
+        this->data_.push_back(tuple_in.data_[index]);
+    }
+}
+
+inline int Tuple::getSize(){
+    return (int)data_.size();
+}
+
+//新增数据
+void Tuple::addData(Data data_in){
+    this->data_.push_back(data_in);
+}
+
+//得到元组中的数据
+std::vector<Data> Tuple::getData(){
+    return this->data_;
+}
+
+void Tuple::showTuple(){
+    for(int index=0;index<getSize();index++){
+        if(data_[index].type==-1)
+            std::cout<<data_[index].values.datai<<'\t';
+        else if(data_[index].type==0)
+            std::cout<<data_[index].values.dataf<<'\t';
+        else
+            std::cout<<*data_[index].values.datas<<'\t';
+    }
+    std::cout<<std::endl;
+}
+
+//table构造函数
+Table::Table(std::string title,Attribute attr){
+    this->title_=title;
+    this->attr_=attr;
+    this->index_.num=0;
+    this->has_key_ = 0;
+    for (int i = 0;i < attr.num;i++) {
+        if (attr.primary[i] == true) {
+            this->has_key_ = 1;
+            break;
+        }
+    }
+}
+
+//table的构造函数，拷贝用
+Table::Table(const Table &table_in){
+    this->attr_=table_in.attr_;
+    this->has_key_=table_in.has_key_;
+    this->index_=table_in.index_;
+    this->title_=table_in.title_;
+    for(int index=0;index<tuple_.size();index++)
+        this->tuple_.push_back(table_in.tuple_[index]);
+}
+
+//插入元组
+// int Table::addTuple(Tuple tuple_in){
+//     if(tuple_in.getSize()!=attr_.num)
+//     {
+//         std::cout<<"Illegal Tuple Insert: The size of column is unequal."<<std::endl;
+//         return 0;
+//     }
+//     for(int index=0;index<attr_.num;index++){
+//         //当输入的tuple的type为int或者float时，必须与对应的attr_.type相同。如果为string的话，则需要不大于attr_.type
+//         if(tuple_in.getData()[index].type>attr_.type[index]||(tuple_in.getData()[index].type<=0&&tuple_in.getData()[index].type!=attr_.type[index]))
+//         {
+//             std::cout<<"Illegal Tuple Insert: The types of attributes are unequal."<<std::endl;
+//             return 0;
+//         }
+//     }
+//     tuple_.push_back(tuple_in);
+//     return 1;
+// }
+
+//插入索引
+int Table::setIndex(short index,std::string index_name){
+    short tmpIndex;
+    for(tmpIndex=0;tmpIndex<index_.num;tmpIndex++){
+        if(index==index_.location[tmpIndex])  //当该元素已经有索引时，报错
+        {
+            std::cout<<"Illegal Set Index: The index has been in the table."<<std::endl;
+            return 0;
+        }
+    }
+    for(tmpIndex=0;tmpIndex<index_.num;tmpIndex++){
+        if(index_name==index_.indexname[tmpIndex])  //当该元素已经有索引时，报错
+        {
+            std::cout<<"Illegal Set Index: The name has been used."<<std::endl;
+            return 0;
+        }
+    }
+    index_.location[index_.num]=index;  //插入索引位置和索引名字，最后把索引个数加一
+    index_.indexname[index_.num]=index_name;
+    index_.num++;
+    return 1;
+}
+
+int Table::dropIndex(std::string index_name){
+    short tmpIndex;
+    for(tmpIndex=0;tmpIndex<index_.num;tmpIndex++){
+        if(index_name==index_.indexname[tmpIndex])  //当该元素已经有索引时，跳出
+            break;
+    }
+    if(tmpIndex==index_.num)
+    {
+        std::cout<<"Illegal Drop Index: No such a index in the table."<<std::endl;
+        return 0;
+    }
+    
+    //交换最后的索引位置和名字，来达到删除的效果
+    index_.indexname[tmpIndex]=index_.indexname[index_.num-1];
+    index_.location[tmpIndex]=index_.location[index_.num-1];
+    index_.num--;
+    return 1;
+}
+
+//计算每个attribute的大小
+// int Table::DataSize(){
+//     int result=0;
+//     for(int index=0;index<attr_.num;index++){
+//         switch (attr_.type[index]) {
+//             case -1:
+//                 result+=sizeof(int);
+//                 break;
+//             case 0:
+//                 result+=sizeof(float);
+//                 break;
+//             default:
+//                 result+=attr_.type[index];
+//                 break;
+//         }
+//     }
+//     return result;
+// }
+
+//返回一些private的值
+inline std::string Table::getTitle(){
+    return title_;
+}
+inline Attribute Table::getAttr(){
+    return attr_;
+}
+std::vector<Tuple>& Table::getTuple(){
+    return tuple_;
+}
+Index Table::getIndex(){
+    return index_;
+}
+short Table::gethasKey(){
+    return has_key_;
+}
+
+void Table::showTable(){
+    for(int index=0;index<attr_.num;index++)
+        std::cout<<attr_.name[index]<<'\t';
+    std::cout<<std::endl;
+    for(int index=0;index<tuple_.size();index++)
+        tuple_[index].showTuple();
+}
---
+++ basic.h
@@ -0,0 +1,100 @@
+//
+//  basic.h
+//  base
+//
+//  Created by Sr on 2017/5/25.
+//  Copyright © 2017年 Sr. All rights reserved.
+//
+
+#ifndef _BASIC_H_
+#define _BASIC_H_ 1
+
+#include <iostream>
+#include <vector>
+
+//用于where的判断 分别为小于，小于等于，等于，大于等于，大于，不等于
+typedef enum{
+    LESS,
+    LESS_OR_EQUAL,
+    EQUAL,
+    GREATER_OR_EQUAL,
+    GREATER,
+    NOT_EQUAL
+} WHERE;
+
+//一个struct存放它的一条信息的类型和值
+//用一个strunt嵌套一个union实现多种类型的转换
+//type的类型：-1：int,0:float,1-255:string(数值为字符串的长度+1),注意使用时对Value的选择！
+struct Data{
+    int type;
+    union Value
+    {
+        int datai;
+        float dataf;
+        std::string* datas;
+    } values;
+};
+
+//Where存放一组判断语句
+struct Where{
+    Data data; //数据
+    WHERE relation_character;   //关系
+};
+
+//在确定类型时，慎用str.size()+1来决定str的type的值，一张表最多32个attribute
+struct Attribute{
+    int num;  //存放table的属性数
+    std::string name[32];  //存放每个属性的名字
+    short type[32];  //存放每个属性的类型，-1：int,0:float,1~255:string的长度+1
+    bool primary[32];  //判断每个属性是否是主键，是为true
+};
+
+//索引管理，一张表最多只能有10个index
+struct Index{
+    int num;  //index的总数
+    short location[10];  //每个index在Attribute的name数组中是第几个
+    std::string indexname[10];  //每个index的名字
+};
+
+//元组管理，数据只增不减
+class Tuple{
+private:
+    std::vector<Data> data_;  //存储元组里的每个数据的信息
+public:
+    Tuple(){};
+    Tuple(const Tuple &tuple_in);  //拷贝元组
+    void addData(Data data_in);  //新增元组
+    std::vector<Data> getData();  //返回数据
+    int getSize();  //返回元组的数据数量
+    void showTuple();  //显示元组中的所有数据
+};
+
+class Table{
+private:
+    std::string title_;  //表名
+    Attribute attr_;  //数据的类型
+    std::vector<Tuple> tuple_;  //存放所有的元组
+    Index index_;  //表的索引信息
+    short has_key_;  //判断是否存在主键,-1为不存在，其他则为主键的所在位置
+public:
+    //构造函数
+    Table(std::string title,Attribute attr);
+    Table(const Table &table_in);
+    
+    // int DataSize();  //每个tuple占的数据大小
+    
+    int setIndex(short index,std::string index_name);  //插入索引，输入要建立索引的Attribute的编号，以及索引的名字，成功返回1失败返回0
+    int dropIndex(std::string index_name);  //删除索引，输入建立的索引的名字，成功返回1失败返回0
+    
+    //private的输出接口
+    std::string getTitle();
+    Attribute getAttr();
+    std::vector<Tuple>& getTuple();
+    Index getIndex();
+    short gethasKey();
+    
+    void showTable(); //显示table的部分数据
+};
+
+
+#endif
