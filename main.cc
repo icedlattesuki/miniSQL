1948b8342a16a98275fad29d5676eab7bd4288db
ShengAndXian <huangyusheng01@gmail.com>
2017-06-17 00:14:58 +0800

    主程序入口

---
+++ main.cc
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  interpreter
+//
+//  Created by Sr on 2017/5/20.
+//  Copyright © 2017年 Sr. All rights reserved.
+//
+
+#include <iostream>
+#include "interpreter.h"
+
+int main(int argc, const char * argv[]) {
+    std::cout<<">>> Welcome to MiniSQL"<<std::endl;
+    while(1){
+        Interpreter query;
+        query.getQuery();
+        query.EXEC();
+    }
+    return 0;
+}
