e59dbab256dbaec4f7e20e03ea2dae466620904e
ShengAndXian <huangyusheng01@gmail.com>
2017-06-17 00:14:20 +0800

    异常更新

--- exception.h
+++ exception.h
@@ -4,43 +4,52 @@
 #include <exception>
 
 class table_exist : public std::exception {
-
+    
 };
 
 class table_not_exist : public std::exception {
-
+    
 };
 
 class attribute_not_exist : public std::exception {
-
+    
 };
 
 class index_exist : public std::exception {
-
+    
 };
 
 class index_not_exist : public std::exception {
-
-};
-
-class index_full : public std::exception{
     
 };
 
 class tuple_type_conflict : public std::exception {
-
+    
 };
 
 class primary_key_conflict : public std::exception {
-
-};
-
-class unique_conflict : public std::exception {
-
+    
 };
 
 class data_type_conflict : public std::exception {
+    
+};
 
+//增加
+class index_full : public std::exception{
+    
+};
+
+class input_format_error : public std::exception{
+    
+};
+
+class exit_command : public std::exception{
+    
+};
+
+class unique_conflict:public std::exception{
+    
 };
 
 #endif 
